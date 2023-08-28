f492bf2dc6c43c795db466ca8b7b6c1a2d0c8064
NotleAnkle <phamdacanh2k2@gmail.com>
2023-08-28 14:06:37 +0700

    + add barrier scripts

---
+++ assets/_Game/Scripts/object/barrier/Barrier.ts
@@ -0,0 +1,31 @@
+import { _decorator, BoxCollider, Component, ICollisionEvent, Node, Vec3} from 'cc';
+import { PlayerController } from '../player/PlayerController';
+const { ccclass, property } = _decorator;
+
+
+
+@ccclass('Barrier')
+export class Barrier extends Component {
+
+    @property(BoxCollider)
+    private collider : BoxCollider = null;
+
+    private _curPos: Vec3 = new Vec3();
+
+    start() {
+        this.node.getPosition(this._curPos);
+        this.collider.on('onCollisionEnter', this.onCollision, this);
+    }
+
+    
+    private onCollision(event: ICollisionEvent) {
+        
+        if(event.otherCollider.node.layer == 1){
+            const player =  event.otherCollider.getComponent(PlayerController);
+            player.goBack(this.node.position);
+        }
+
+    }
+}
+
+
